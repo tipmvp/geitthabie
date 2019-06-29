ede5501270dc45e45abb2a1b0509d9131042bad4
ttp <527813949@qq.com>
2019-06-29 15:03:35 +0800

    第一次修改

---
+++ src/main/java/Userapp/AppUser.java
@@ -0,0 +1,12 @@
+package Userapp;
+
+/**
+ * @author XRZ
+ * @date 2019/6/29
+ * @Description :
+ */
+public class AppUser {
+    public static void main(String[] args) {
+        System.out.println("githaub");
+    }
+}
